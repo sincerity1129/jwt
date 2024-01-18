8147bb11376ff4ca92fa287de768cd4db2cecf0c
unwoo <sincerity1129@naver.com>
2024-01-18 18:09:42 +0900

    fix jwt

--- example.py
+++ example.py
@@ -5,5 +5,5 @@ name = 'test'
 password = '1234'
 
 user_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())  # 비밀번호 저장
-user_token = jwt.encode({'name': name}, password, algorithm="HS256")  # str으로 반환하여 return
+user_token = jwt.encode({'name': name}, user_password, algorithm="HS256")  # str으로 반환하여 return
 user_check = bcrypt.checkpw(password.encode('utf-8'), user_password)  # 비밀번호 일치 확인
